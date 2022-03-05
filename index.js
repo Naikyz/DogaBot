4ee16b1c0dcd04f5904285477f315c58b74ccbb9
Mr.Slooth <victor.guyot@epitech.eu>
2022-03-06 00:09:13 +0100

    feat: docjerize bit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Require the necessary discord.js classes
 const { Client, Intents } = require('discord.js');
-const { token } = require('./config.json');
+const TOKEN = process.env.TOKEN;
 const axios = require("axios");
 
 // Create a new client instance
@@ -20,13 +20,10 @@ async function getInfo() {
 			}`
 		}
 	  })
-	//   console.log(response.data.data.fa[0].floor_price)
 	  return (response.data.data.fa[0].floor_price.toString());
 }
-// Login to Discord with your client's token
-client.login(token);
-
 client.on('ready', () => {
+	console.log("READY")
 	getInfo().then(function infos (result) {
 		client.user.setActivity("Floor Price: " + result.slice(0, 3) + "," + result.slice(3, 5) + "ꜩ");
 	});
@@ -36,3 +33,5 @@ client.on('ready', () => {
 		});
 	}, 120000);
 })
+
+client.login(TOKEN);
